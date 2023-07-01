9fdd66f99f1d59e7b4fbc7a3282a2dd51ab9f124
HyeonGyu Park <gusrb1502@gmail.com>
2023-07-02 03:07:57 +0900

    Initial commit

---
+++ src/main/java/com/sparta/plug/PlugApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.plug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/plug/PlugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.plug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
